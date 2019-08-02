438bd2437c356a2418d72348952569dbcbd7bd2e
ZenekeZene <hectorvillarm@gmail.com>
2019-08-02 11:22:48 +0200

    Revert "7 passing, 1 failing"
    This reverts commit 6703b0f7a753faedaa1b5b7e25a8032b001777fb.

--- app/katayuno.js
+++ app/katayuno.js
@@ -1,25 +1,8 @@
 export default class Katayuno {
-  calculatePrice(booksToBuy) {
-    let price = 0;
-    booksToBuy.forEach(book => {
-      price += book.units * 8;
-    });
-	price -= price * this.calculatePercentDiscount(booksToBuy.length);
 
-    return price;
-  }
 
-  calculatePercentDiscount(size) {
-    let discount = 0;
-    if (size === 2) {
-		discount = 5;
-    } else if (size === 3) {
-		discount = 10;
-	} else if (size === 4) {
-		discount = 20;
-	} else if (size === 5) {
-		discount = 25;
-	}
-	return discount / 100;
-  }
+    sum(a, b) {
+        return 5;
+    }
 }
+
